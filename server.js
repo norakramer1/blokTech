2f9d4a9676913a81bfb38686cb81cd2438b0c1a9
norakramer1 <nora.kramer2@hva.nl>
2021-03-08 20:10:08 +0100

    mongodb poging en css aanpassingen

--- server.js
+++ server.js
@@ -1,31 +1,64 @@
 const _ = require('lodash');
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
+const mongoDB= require('mongodb');
+require('dotenv').config()
+var assert = require('assert');
+
+const uri = process.env.DB_URI;
+var db = null
+let favBoeken;
+
+app.post('/profiel', (req, res) =>{
+  favBoeken.insertOne, ('Harry Potter')
+})
+
+ mongoDB.MongoClient.connect(uri, function (err, client) {
+   db = client.db("test")
+   favBoeken = db.collection('favBoeken');
+   assert.equal(null, err);
+   client.close();
+ });
+
+
 const port = 3000;
 const path = require('path');
 
+app.use(express.urlencoded({ extended: true }));
+
+
+
+// view engine //
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, './views'));
 
 app.use('/static', express.static(path.join(__dirname, 'static')))
 
+
+
+
 app.get('/', (req, res) => {
-  // res.send('Hello World!')
-  res.render('index');
+  res.render('index')
 });
 
-app.get('/about', (req, res) => {
- res.send('about')
+app.get('/matches', (req, res) => {
+ res.render('matches')
 
 });
 
-app.get('/login', (req, res) => {
-  res.send('login')
+app.get('/profiel', (req, res) => {
+  res.render('profiel')
 });
 
 app.get("*", (req, res) => {
   res.send('404 Not Found sorry!')
 });
+
+app.post('/profiel', (req, res) =>{
+console.log(req.body);
+})
+
 
 
 
