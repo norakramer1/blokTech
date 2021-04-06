3282c83a42f81801be1bd75a2ae2163221081bb0
nora kramer <nora.kramer2@hva.nl>
2021-04-06 13:22:17 +0200

    css + addboeken toegevoegd
    css voor pagina's aangepast, boeken nu ook aan db toevoegen

--- server.js
+++ server.js
@@ -31,6 +31,17 @@ app.use('/static', express.static(path.join(__dirname, 'static')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
+// // finds matching account based on form input
+// app.post('/profiel', async (req, res) => {
+//     const findBook = await favBook.find({
+//       book1: req.body.book1,
+//       book2: req.body.book2,
+//       book3: req.body.book3,
+//
+//     });
+//
+//     res.render('matches', {findBook: findBook, name: req.body.name});
+//
 
 // app.get('/', async (req, res) => {
 //    const findBook = await favBook.find();
@@ -39,26 +50,56 @@ app.use(bodyParser.urlencoded({extended: true}));
 //  });
 
 
+
+// finds matching account based on form input and adds account to db
 app.post('/profiel', async (req, res) => {
-       const findBook = await favBook.find ({
-           book1: req.body.book1,
-           book2: req.body.book2,
-           book3: req.body.book3,
-
-      });
-
-
-       res.render('partials/profielen', { favBooks: findBook });
-       // console.log(findBook)
-
+  const findBook = await favBook.find({
+    book1: req.body.book1,
+    book2: req.body.book2,
+    book3: req.body.book3,
 
   });
+  const addBook = await new favBook({
+      book1: req.body.book1,
+      book2: req.body.book2,
+      book3: req.body.book3,
+
+    });
+    addBook.save(function (err,newBook){
+      if (err) {
+        console.log(err)
+      } else {
+      res.render ('matches', {findBook: findBook})
+
+    }
+  });
+  });
+
+// res.render('matches', {
+//   findBook: findBook
+// });
 
 
+// add books to database
+// app.post('/profiel', async (req, res) => {
+//   const addBook = new favBook({
+//     book1: req.body.book1,
+//     book2: req.body.book2,
+//     book3: req.body.book3,
+//
+//   });
+//   addBook.save(function (err,newBook){
+//     if (err) {
+//       console.log(err)
+//     } else {
+//     res.render ('matches')
+//
+//   }
+// });
+// });
 
 
-
-
+// routes
 app.get('/', (req, res) => {
   res.render('index')
 });
@@ -75,10 +116,6 @@ app.get('/profiel', (req, res) => {
 app.get("*", (req, res) => {
   res.send('404 Not Found sorry!')
 });
-
-// app.post('/profiel', (req, res) =>{
-// console.log(req.body);
-// });
 
 
 
